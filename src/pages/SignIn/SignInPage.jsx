e9e1ce0c884a61feebe2116068a863f3154dea28
lucas-lima-dev <lucas.sdslima@gmail.com>
2023-06-04 17:44:24 -0300

    refactor: styling singIn page using windstich

--- src/pages/SignIn/SignInPage.jsx
+++ src/pages/SignIn/SignInPage.jsx
@@ -1,37 +1,36 @@
 import { useContext, useState } from "react";
-import { SignInContainer } from "./styled";
+import {
+  SignInPageContainer,
+  TitleContainer,
+  FormContainer,
+  StyledForm,
+  StyledInput,
+  StyledButton,
+  StyledP
+} from "./styled";
 
 function SignInPage() {
-    //   const navigate = useNavigate();
+  //   const navigate = useNavigate();
 
-    return(
-        <div className="bg-red-700 w-screen h-screen flex ">
-            <div className="bg-black font-bold font-PassionOne text-white flex flex-col w-[75vw]">
-                <div className="m-[220px 0 0 110px]">
-                    <h1 className="text-[106px] ">FunForFamily</h1>
-                    <h2>Discover where to go with yours kids</h2>
-                </div>
-            </div>
-
-            <div>
-                <form>
-                    <input
-                    type="email"
-                    placeholder="e-mail"
-                    required
-                    />
-                    <input
-                    type="password"
-                    placeholder="password"
-                    required
-                    />
-                    <button type="submit">Sign In</button>
-                    <p>First time? Create an account!</p>
-                </form>
-            </div>
+  return (
+    <SignInPageContainer>
+      <TitleContainer>
+        <div className="m-[220px 0 0 110px]">
+          <h1 className="text-[106px] ">FunForFamily</h1>
+          <h2 className="font-bold w-[442px] text-[43px]">Discover where to go with yours kids</h2>
         </div>
-    )
-}
+      </TitleContainer>
 
+      <FormContainer>
+        <StyledForm>
+          <StyledInput type="email" placeholder="e-mail" required />
+          <StyledInput type="password" placeholder="password" required />
+          <StyledButton type="submit">Sign In</StyledButton>
+          <StyledP>First time? Create an account!</StyledP>
+        </StyledForm>
+      </FormContainer>
+    </SignInPageContainer>
+  );
+}
 
 export default SignInPage;
--- src/pages/SignIn/styled.js
+++ src/pages/SignIn/styled.js
@@ -1,7 +1,68 @@
-import { w } from "windstitch"
+import { w } from "windstitch";
 
-export const SignInContainer = w.div(
+export const SignInPageContainer = w.div(
+  `
+     w-screen h-screen flex 
     `
-    bg-red
+);
+
+export const TitleContainer = w.div(
+  `
+    bg-black 
+    font-bold 
+    font-PassionOne 
+    text-white 
+    flex 
+    flex-col 
+    w-[75vw] 
+    flex-grow 
+    justify-center 
+    items-center
+    `
+);
+
+export const FormContainer = w.div(
+  `
+    flex flex-col w-[50vw] bg-[#333333] justify-center items-center
+    `
+);
+
+export const StyledForm = w.form(
+  `
+    m-auto flex flex-col justify-center items-center gap-[10px]
+    `
+);
+
+export const StyledInput = w.input(
+  `
+    w-[30vw] 
+    h-[65px] 
+    bg-[#FFFFFF] 
+    border-[1px] 
+    border-white 
+    rounded-[6px] 
+    text-[#9f9f9f] 
+    text-[20px] 
+    p-[18px]
+    `
+);
+
+export const StyledButton = w.button(
+    `
+    w-[30vw]
+    h-[65px]
+    bg-[#1877f2]
+    rounded-[6px]
+    text-[#FFFFFF]
+    text-[27px]
+    `
+);
+
+export const StyledP = w.p(
+    `
+    text-[#FFFFFF]
+    text-[20px]
+    cursor-pointer
+    underline
     `
 )
